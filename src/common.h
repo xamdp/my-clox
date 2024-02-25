446a4be4db54b75a6e7c1bf269fbb363e638a3f3
xamdp <diegoysmaeldocot@gmail.com>
2024-02-26 00:51:13 +0800

    add common.h

---
+++ src/common.h
@@ -0,0 +1,9 @@
+/* Amending the soil :) */
+#ifndef clox_common_h	/* Checks if macro clox_common_h is not defined */
+#define clox_common_h
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#endif
