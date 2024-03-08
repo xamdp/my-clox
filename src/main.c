5d0c2a8ccc4df547cf47bc7dbf250f0e433dadc6
xamdp <diegoysmaeldocot@gmail.com>
2024-03-08 18:29:28 +0800

    modify main.c

--- src/main.c
+++ src/main.c
@@ -11,7 +11,7 @@ int main(int argc, const char* argv[]) {
 	writeChunk(&chunk, OP_CONSTANT, 123); /* Then we write the constant instruction, starting with its opcode.*/	
 	writeChunk(&chunk, constant, 123); /* After that, we write the one-byte constant index operand*/
 
-	writeChunk(&chunk, OP_RETURN, 123);
+	writeChunk(&chunk, OP_RETURN, 0);
 	disassembleChunk(&chunk, "test chunk");
 	freeChunk(&chunk);
 	return 0;
